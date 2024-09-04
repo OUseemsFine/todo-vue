89603186d3e29928cad56142ec2ad7ca9377c80a
jundong <1966273343@qq.com>
2024-09-04 10:13:40 +0800

    1. CSS added 2. compute field is added

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ return {
   TodoItems:[
     {id:"todo-"+nanoid(), label: "Task 1" , done: false},
     {id:"todo-"+nanoid(), label: "Task 2" , done: false},
-    {id:"todo-"+nanoid(), label: "Task 3" , done: false},
+    {id:"todo-"+nanoid(), label: "Task 3" , done: true},
     {id:"todo-"+nanoid(), label: "Task 4" , done: false},
 ],
 };
@@ -24,7 +24,21 @@ return {
      addTodo(todoLabel){
       this.TodoItems.push({id: "todo-" + nanoid(), label: todoLabel, done: false});
      },
+
+     updateDoneStatus(toDoId) {
+  const toDoToUpdate = this.TodoItems.find((item) => item.id === toDoId)
+  toDoToUpdate.done = !toDoToUpdate.done
+}
+
   },
+
+  computed: {
+  listSummary() {
+    const numberFinishedItems = this.TodoItems.filter((item) =>item.done).length;
+    return `${numberFinishedItems} out of ${this.TodoItems.length} items completed`;
+  }
+}
+
 };
 
 </script>
@@ -35,43 +49,134 @@ return {
       To-Do List
     </h1>
     <todo-form @todo-added="addTodo"></todo-form>
-      <ul>
+    <h2 id="list-summary">
+      {{listSummary}}
+    </h2>
+      <ul aria-labelledby="list-summary" class="stack-large">
         <li v-for="item in TodoItems"  v-bind:key="item.id">
           <todo-item
           v-bind:label="item.label" 
           v-bind:id="item.id"
-          v-bind:done="item.done">
+          v-bind:done="item.done"
+          @checkbox-changed="updateDoneStatus(item.id)">
         </todo-item>
         </li>
       </ul>
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+<style>
+  /* Global styles */
+  .btn {
+    padding: 0.8rem 1rem 0.7rem;
+    border: 0.2rem solid #4d4d4d;
+    cursor: pointer;
+    text-transform: capitalize;
+  }
+  .btn__danger {
+    color: #fff;
+    background-color: #ca3c3c;
+    border-color: #bd2130;
+  }
+  .btn__filter {
+    border-color: lightgrey;
+  }
+  .btn__danger:focus {
+    outline-color: #c82333;
+  }
+  .btn__primary {
+    color: #fff;
+    background-color: #000;
+  }
+  .btn-group {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: space-between;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .btn-group > * {
+    flex: 1 1 auto;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .btn-group > * + * {
+    margin-left: 0.8rem;
   }
-}
+  .label-wrapper {
+    margin: 0;
+    flex: 0 0 100%;
+    text-align: center;
+  }
+  [class*="__lg"] {
+    display: inline-block;
+    width: 100%;
+    font-size: 1.9rem;
+  }
+  [class*="__lg"]:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+  @media screen and (min-width: 620px) {
+    [class*="__lg"] {
+      font-size: 2.4rem;
+    }
+  }
+  .visually-hidden {
+    position: absolute;
+    height: 1px;
+    width: 1px;
+    overflow: hidden;
+    clip: rect(1px 1px 1px 1px);
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: rect(1px, 1px, 1px, 1px);
+    white-space: nowrap;
+  }
+  [class*="stack"] > * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .stack-small > * + * {
+    margin-top: 1.25rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.5rem;
+  }
+  @media screen and (min-width: 550px) {
+    .stack-small > * + * {
+      margin-top: 1.4rem;
+    }
+    .stack-large > * + * {
+      margin-top: 2.8rem;
+    }
+  }
+  /* End global styles */
+  #app {
+    background: #fff;
+    margin: 2rem 0 4rem 0;
+    padding: 1rem;
+    padding-top: 0;
+    position: relative;
+    box-shadow:
+      0 2px 4px 0 rgb(0 0 0 / 20%),
+      0 2.5rem 5rem 0 rgb(0 0 0 / 10%);
+  }
+  @media screen and (min-width: 550px) {
+    #app {
+      padding: 4rem;
+    }
+  }
+  #app > * {
+    max-width: 50rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  #app > form {
+    max-width: 100%;
+  }
+  #app h1 {
+    display: block;
+    min-width: 100%;
+    width: 100%;
+    text-align: center;
+    margin: 0;
+    margin-bottom: 1rem;
+  }
 </style>
+
+
+
--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,8 +1,10 @@
 <template>
 <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
     <label for="new-todo-input">
         What needs to be done?
     </label>
+    </h2>
     <input 
     type="text"
     id="new-todo-input"
@@ -10,7 +12,7 @@
     autocomplete="off"
     v-model.lazy.trim="label"
     />
-    <button type="submit">
+    <button type="submit" class="btn btn__primary btn__lg">
         Add
     </button>
 </form>
